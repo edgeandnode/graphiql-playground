1600d8c803b1bacc4d75e6f97648a663a5b7cd2c
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2022-10-04 14:34:05 +0800

    Fix DefaultQueryChip spacing

--- src/SavedQueriesToolbar/DefaultQueryChip.tsx
+++ src/SavedQueriesToolbar/DefaultQueryChip.tsx
@@ -6,24 +6,26 @@ export interface DefaultQueryChipProps {
   visible: boolean
 }
 export function DefaultQueryChip({ visible }: DefaultQueryChipProps) {
-  // We want to preserve the space for the chip even when it's not visible.
-  if (!visible) {
-    return <div sx={{ width: '56px' }} />
-  }
-
   return (
-    <Chip
-      sx={{
-        fontSize: FontSize['12px'],
-        py: Spacing['4px'],
-        px: Spacing['8px'],
-        pointerEvents: 'none',
-        userSelect: 'none',
-        fontWeight: FontWeight['LIGHT'],
-        borderRadius: BorderRadius['S'],
-      }}
-    >
-      Default
-    </Chip>
+    /* We want to preserve the space for the chip even when it's not visible,
+       and we can't pass `sx.width` to Chip because of the tooltip, thus
+       we have this container div. */
+    <div sx={{ width: '56px', flexShrink: 0 }}>
+      {!!visible && (
+        <Chip
+          sx={{
+            fontSize: FontSize['12px'],
+            py: Spacing['4px'],
+            px: Spacing['8px'],
+            pointerEvents: 'none',
+            userSelect: 'none',
+            fontWeight: FontWeight['LIGHT'],
+            borderRadius: BorderRadius['S'],
+          }}
+        >
+          Default
+        </Chip>
+      )}
+    </div>
   )
 }
