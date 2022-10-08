017a057a09dedce7d364da0c1f1cdf37e03cf4a8
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2022-10-08 15:05:14 +0800

    Tell the users why are the buttons disabled

--- src/SavedQueriesToolbar/SavedQueriesActionButtons.tsx
+++ src/SavedQueriesToolbar/SavedQueriesActionButtons.tsx
@@ -1,10 +1,16 @@
 /** @jsxImportSource theme-ui */
 
-import { Flex, NewGDSButton as Button, Spacing } from '@edgeandnode/components'
+import { Flex, NewGDSButton as Button, Spacing, Tooltip } from '@edgeandnode/components'
 
 import { SnackbarMessageType } from './messages'
 import { SavedQuery } from './types'
 import { validateQuery, ValidationStatus } from './validation'
+
+const disableButtonReasonTooltips = {
+  saveAsNew: 'A new query must have a different name.',
+  save: 'The query must have any changes to save them.',
+  cancel: 'The query must have any changes to cancel them.',
+}
 
 export interface SavedQueriesActionButtonsProps<TQuery extends SavedQuery> {
   currentQuery: TQuery | null
@@ -81,47 +87,53 @@ export function SavedQueriesActionButtons<TQuery extends SavedQuery>({
       className={className}
     >
       {currentQuery && (
-        <Button
-          size="medium"
-          variant="tertiary"
-          disabled={!anythingChanged}
-          onClick={() => {
-            const name = queryNameDraft || currentQuery.name
+        <Tooltip disabled={anythingChanged} text={disableButtonReasonTooltips.save}>
+          <Button
+            size="medium"
+            variant="tertiary"
+            disabled={!anythingChanged}
+            onClick={() => {
+              const name = queryNameDraft || currentQuery.name
 
-            const validationStatus: ValidationStatus = validateQuery({
-              name,
-              updatedId: currentQuery.id,
-              queries: queries,
-              query: querySourceDraft,
-            })
-
-            if (validationStatus !== 'valid') {
-              setSnackbarMessage(validationStatus)
-              return
-            }
-
-            void onUpdateQuery({
-              name,
-              query: querySourceDraft,
-            })
-              .then(() => {
-                setSnackbarMessage('success-update')
+              const validationStatus: ValidationStatus = validateQuery({
+                name,
+                updatedId: currentQuery.id,
+                queries: queries,
+                query: querySourceDraft,
               })
-              .catch(() => {
-                setSnackbarMessage('error-update')
+
+              if (validationStatus !== 'valid') {
+                setSnackbarMessage(validationStatus)
+                return
+              }
+
+              void onUpdateQuery({
+                name,
+                query: querySourceDraft,
               })
-          }}
-        >
-          Save
-        </Button>
+                .then(() => {
+                  setSnackbarMessage('success-update')
+                })
+                .catch(() => {
+                  setSnackbarMessage('error-update')
+                })
+            }}
+          >
+            Save
+          </Button>
+        </Tooltip>
       )}
-      <Button size="medium" variant="tertiary" onClick={() => void handleSaveAsNewClick()} disabled={!nameChanged}>
-        Save as new
-      </Button>
-      <Button size="medium" variant="tertiary" onClick={onResetChanges} disabled={!anythingChanged}>
-        {/* Reset changes */}
-        Cancel
-      </Button>
+      <Tooltip disabled={nameChanged} text={disableButtonReasonTooltips.saveAsNew}>
+        <Button size="medium" variant="tertiary" onClick={() => void handleSaveAsNewClick()} disabled={!nameChanged}>
+          Save as new
+        </Button>
+      </Tooltip>
+      <Tooltip disabled={anythingChanged} text={disableButtonReasonTooltips.cancel}>
+        <Button size="medium" variant="tertiary" onClick={onResetChanges} disabled={!anythingChanged}>
+          {/* Reset changes */}
+          Cancel
+        </Button>
+      </Tooltip>
     </Flex>
   )
 }
