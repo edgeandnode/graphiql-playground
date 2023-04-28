dc9b54506af30a7f16d458901c55e2f574e165dc
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-04-28 12:09:50 +0200

    Remove a redundant comment

--- src/useQuerySource.tsx
+++ src/useQuerySource.tsx
@@ -1,10 +1,6 @@
 import { useEditorContext } from '@graphiql/react'
 import { useEffect, useRef, useState } from 'react'
 
-/**
- * We don't want to rerender the whole component every time the query changes,
- * because it leads to bugs like: https://github.com/edgeandnode/graphiql-playground/pull/15
- */
 export function useQuerySource() {
   const { queryEditor } = useEditorContext()!
   const [state, setState] = useState(queryEditor?.getValue() || '')
